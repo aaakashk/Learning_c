ca769d0bbbb09f3f8026f298ae498e7d26d41dcf
Akash Kumar <akashyadav112314@gmail.com>
2022-01-04 23:30:13 +0530

    mid sem question print same data

---
+++ print_same_data.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+
+typedef struct node {
+    int data;
+    struct node* next;
+} nodetype;
+
+nodetype* create_p(nodetype*, int);
+nodetype* create_q(nodetype*, int);
+void display_p(nodetype*);
+void display_q(nodetype*);
+void display_same_data(nodetype*, nodetype*);
+int main() {
+    nodetype *p_head = NULL, *q_head = NULL;
+    int number1, number2, n;
+    printf("Enter the number of nodes of p list.");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter the number: ");
+        scanf("%d", &number1);
+        p_head = create_p(p_head, number1);
+    }
+    printf("P: ");
+    display_p(p_head);
+
+    printf("Enter the number of nodes of q list.");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        printf("Enter the number: ");
+        scanf("%d", &number2);
+        q_head = create_q(q_head, number2);
+    }
+    printf("Q: ");
+    display_q(q_head);
+
+    printf("=========================================\n");
+    display_same_data(p_head, q_head);
+}
+
+nodetype* create_p(nodetype* start, int num) {
+    nodetype* ptr = (nodetype*)malloc(sizeof(nodetype));
+    ptr->data = num;
+    ptr->next = NULL;
+    if (start == NULL)
+        start = ptr;
+    else {
+        nodetype* temp = start;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = ptr;
+    }
+    return start;
+}
+
+nodetype* create_q(nodetype* start, int num) {
+    nodetype* ptr = (nodetype*)malloc(sizeof(nodetype));
+    ptr->data = num;
+    ptr->next = NULL;
+    if (start == NULL)
+        start = ptr;
+    else {
+        nodetype* temp = start;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = ptr;
+    }
+    return start;
+}
+
+void display_p(nodetype* start) {
+    while (start != NULL) {
+        printf("%d ", start->data);
+        start = start->next;
+    }
+    printf("\n");
+}
+
+void display_q(nodetype* start) {
+    while (start != NULL) {
+        printf("%d ", start->data);
+        start = start->next;
+    }
+    printf("\n");
+}
+
+void display_same_data(nodetype* p_head, nodetype* q_head) {
+    while (p_head != NULL) {
+        nodetype* s = q_head;
+        while (s != NULL) {
+            if (s->data == p_head->data)
+                printf("%d ", p_head->data);
+            s = s->next;
+        }
+        p_head = p_head->next;
+    }
+}
