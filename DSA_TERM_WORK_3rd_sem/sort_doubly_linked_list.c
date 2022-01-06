9a83d40f2fce9bc8826ef0c52f42a930809fb73c
Akash Kumar <akashyadav112314@gmail.com>
2022-01-06 22:51:51 +0530

    sort doubly linked list

---
+++ DSA_TERM_WORK_3rd_sem/sort_doubly_linked_list.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node* next;
+    struct node* prev;
+} nodetype;
+
+void insert(nodetype**, nodetype**);
+void display(nodetype*);
+void count_nodes(nodetype*, int*);
+//void sort(nodetype**, nodetype**);
+
+int main() {
+    nodetype *head = NULL, *tail = NULL;
+    int choice, count = 0;
+    do {
+        printf(
+            "1. Insert a number.\n"
+            "2. Display the list.\n"
+            "3. Sort the list and display.\n"
+            "4. Exit.\n"
+            "5. Count the nodes.\n"
+            "Enter your choice: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                insert(&head, &tail);
+                break;
+            case 2:
+                if (head == NULL && tail == NULL)
+                    printf("The list is empty.\n");
+                else {
+                    display(head);
+                }
+                break;
+            case 5:
+                count_nodes(head, &count);
+                printf("Count = %d\n", count);
+                break;
+            // case 3:
+            //     if (head == NULL && tail == NULL)
+            //         printf("The list is empty.\n");
+            //     else {
+            //         sort(&head, &tail);
+            //         printf("The sorted list is : \n");
+            //         display(head);
+            //     }
+            //     break;
+            case 4:
+                exit(0);
+            default:
+                printf("Wrong choice. Enter again.\n");
+        }
+    } while (1);
+}
+
+void insert(nodetype** h, nodetype** t) {
+    nodetype* ptr = (nodetype*)malloc(sizeof(nodetype));
+    if (ptr == NULL)
+        printf("Not enough memory.\n");
+    else {
+        int num;
+        printf("Enter the number: ");
+        scanf("%d", &num);
+        ptr->data = num;
+        ptr->next = NULL;
+        ptr->prev = NULL;
+        if (*h == NULL)
+            *h = *t = ptr;
+        else {
+            (*t)->next = ptr;
+            ptr->prev = *t;
+            *t = ptr;
+        }
+    }
+}
+
+void display(nodetype* start) {
+    while (start != NULL) {
+        printf("%d ", start->data);
+        start = start->next;
+    }
+    printf("\n");
+}
+
+void count_nodes(nodetype* h, int* c) {
+    while (h != NULL) {
+        (*c)++;
+        h = h->next;
+    }
+}
+
+//void sort(nodetype** h, nodetype** t) {
+//}
